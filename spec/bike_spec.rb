ca5245f30a1141b9dc54b8953d23de3a2144107c
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-10-29 13:45:16 +0000

    Refactored tests, cleaned up code

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,22 +1,20 @@
 require 'bike'
 
 describe Bike do
-  context 'when working? is called' do
-    it 'responds to the method working?' do
+  context '#initialize' do
+    let(:bike) { Bike.new(false) }
+    it 'allows user to set working or broken' do
+      expect(bike.working).to eq(false)
+    end
+  end
+
+  context '#working?' do
+    it 'can be used on a Bike object' do
       expect(subject).to respond_to(:working?)
     end
-  end
 
-  context 'when working? is called' do
     it 'expects bike to be working' do
       expect(subject.working?).to eq(true)
-    end
-  end
-
-  context 'when class is instantiated' do
-    let(:bike) { Bike.new(false) }
-    it 'allows user to set working attribute' do
-      expect(bike.working).to eq(false)
     end
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,70 +2,57 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
-
-  context 'when release_bike is called' do 
-    it 'responds to the method release_bike' do
-      expect(subject).to respond_to(:release_bike)
-    end
-  end
-
-  context 'when release_bike is called' do
-    it 'returns a bike instance' do
-      subject.dock(double(:bike))
-      expect(subject.release_bike).to be_instance_of(Bike)
-    end
-  end
-
-  context 'when dock is called' do
-    it 'pushes a bike into the bikes array' do
-      expect { subject.dock(double(:bike) }.to change { subject.bikes.length }.by(1)
-    end
-  end
-
-  context 'when public bikes attr is accessed' do
-    it 'shows all the available bikes' do
-      expect(subject.bikes).to be_instance_of(Array)
-    end
-  end
-
-  context 'when release_bikes is called' do
-    it 'raises an error when there are no bikes' do
-      expect { subject.release_bike }.to raise_error(RuntimeError)
-    end
-  end
-
-  context 'when dock is called' do 
-    it 'raises an error if capacity is full' do
-      DockingStation::DEFAULT_CAPACITY.times { subject.dock(double(:bike)) }
-      expect { subject.dock(double(:bike)) }.to raise_error(RuntimeError)
-    end
-  end
-
-  context 'when class is instantiated' do
+  context '#initialize' do
     let(:station) { DockingStation.new(77) }
     it 'allows user to set a capacity instance variable' do
       expect(station.capacity).to eq(77)
     end
-  end
 
-  context 'when class is instantiated' do
     it "default capacity of 20 is set if user doesn't set a capacity" do
       expect(subject.capacity).to eq(20)
     end
   end
 
-  context 'when release bikes is called' do
+  context '#release_bike' do 
+    it 'can be called on a DockingStation object' do
+      expect(subject).to respond_to(:release_bike)
+    end
+
+    it 'raises an error when there are no bikes' do
+      expect { subject.release_bike }.to raise_error(RuntimeError)
+    end
+
+    it 'raises an error if all bikes are broken' do
+      5.times { subject.dock(Bike.new(false)) }
+      expect { subject.release_bike }.to raise_error('All bikes currently out of order')
+    end
+
+    it 'returns a bike instance' do
+      subject.dock(Bike.new)
+      expect(subject.release_bike).to be_instance_of(Bike)
+    end
+
     it "doesn't release a bike that is broken" do
-      3.times { subject.dock(double(:bike)(false)) }
-      2.times { subject.dock(double(:bike)) }
+      3.times { subject.dock(Bike.new(false)) }
+      2.times { subject.dock(Bike.new) }
       expect(subject.release_bike.working?).to eq(true)
     end
   end
 
-  context 'when release bikes is called' do
-    it 'raises error if all bikes are broken' do
-      5.times { subject.dock(double(:bike)(false)) }
-      expect { subject.release_bike }.to raise_error "All bikes currently out of order"
+  context '#dock' do
+    it 'raises an error if capacity is full' do
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock(Bike.new) }
+      expect { subject.dock(Bike.new) }.to raise_error('Docking station full')
+    end
+
+    it 'pushes a bike into the bikes array' do
+      expect { subject.dock(Bike.new) }.to change { subject.bikes.length }.by(1)
+    end
+  end
+
+  context 'bikes attr reader' do
+    it 'shows all the available bikes' do
+      expect(subject.bikes).to be_instance_of(Array)
     end
   end
 end
