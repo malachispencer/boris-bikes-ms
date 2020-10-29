0191ca5323023c7d21de9c86533f529c7a8498e2
Malachi Spencer <m.spencer@live.co.uk>
2020-10-29 08:10:20 +0000

    Replaced all Bike.new test instances with double(:bike), tests now failing

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,7 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
+
   context 'when release_bike is called' do 
     it 'responds to the method release_bike' do
       expect(subject).to respond_to(:release_bike)
@@ -10,14 +11,14 @@ describe DockingStation do
 
   context 'when release_bike is called' do 
     it 'returns a bike instance' do
-      subject.dock(Bike.new)
+      subject.dock(double(:bike))
       expect(subject.release_bike).to be_instance_of(Bike)
     end
   end
 
   context 'when dock is called' do
     it 'pushes a bike into the bikes array' do
-      expect { subject.dock(Bike.new) }.to change { subject.bikes.length }.by(1)
+      expect { subject.dock(double(:bike) }.to change { subject.bikes.length }.by(1)
     end
   end
 
@@ -35,8 +36,8 @@ describe DockingStation do
 
   context 'when dock is called' do 
     it 'raises an error if capacity is full' do
-      DockingStation::DEFAULT_CAPACITY.times { subject.dock(Bike.new) }
-      expect { subject.dock(Bike.new) }.to raise_error(RuntimeError)
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock(double(:bike)) }
+      expect { subject.dock(double(:bike)) }.to raise_error(RuntimeError)
     end
   end
 
@@ -55,16 +56,16 @@ describe DockingStation do
 
   context 'when release bikes is called' do
     it "doesn't release a bike that is broken" do
-      3.times { subject.dock(Bike.new(false)) }
-      2.times { subject.dock(Bike.new) }
+      3.times { subject.dock(double(:bike)(false)) }
+      2.times { subject.dock(double(:bike)) }
       expect(subject.release_bike.working?).to eq(true)
     end
   end
 
   context 'when release bikes is called' do
     it 'raises error if all bikes are broken' do
-      5.times { subject.dock(Bike.new(false)) }
-      expect{ subject.release_bike }.to raise_error "All bikes currently out of order"
+      5.times { subject.dock(double(:bike)(false)) }
+      expect { subject.release_bike }.to raise_error "All bikes currently out of order"
     end
   end
 end
