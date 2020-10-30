8a2df37c6fc48db338eb85e7a79b30874cf5a876
Malachi Spencer <m.spencer@live.co.uk>
2020-10-30 11:43:17 +0000

    Red: Added test for repair_bikes method

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -12,8 +12,27 @@ describe Garage do
       2.times { station.dock(broken_bike) }
       station.ready_for_repair
       van.transport_broken(station, subject)
+
       subject.garage_bikes.each do |bike|
         expect(bike.working).to eq(false)
+      end
+    end
+
+    context '#repair_bikes' do
+      it 'repairs all the bikes in the garage_bikes array' do
+        station = DockingStation.new(0)
+        van = Van.new
+        broken_bike = double('broken_bike', :working => false)
+        working_bike = double('working_bike', :working => true)
+        3.times { station.dock(working_bike) }
+        2.times { station.dock(broken_bike) }
+        station.ready_for_repair
+        van.transport_broken(station, subject)
+        subject.repair_bikes
+        
+        subject.garage_bikes.each do |bike|
+          expect(bike.working).to eq(true)
+        end
       end
     end
   end
