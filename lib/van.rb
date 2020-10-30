32b00cde2f3527bd911b3be28c8ae578163132c4
Malachi Spencer <m.spencer@live.co.uk>
2020-10-30 10:38:01 +0000

    Green: Added raise error line in transport_broken method, test passes

--- lib/van.rb
+++ lib/van.rb
@@ -2,9 +2,12 @@ require_relative './docking_station'
 
 class Van
   def transport_broken(station, garage)
+    raise 'No broken bikes' if station.broken_bikes.empty?
+
     until station.broken_bikes.empty? do
       garage << station.broken_bikes.shift
     end
+    
     garage
   end
 end
