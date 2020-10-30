a2d29a8802b80c188b0a61f8a622ae7771a84726
Malachi Spencer <m.spencer@live.co.uk>
2020-10-30 10:23:41 +0000

    Added test that ensures van method removes broken bikes from DockingStation

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -3,7 +3,18 @@ require 'docking_station'
 
 describe Van do
   context '#transport_broken' do
-    it 'moves broken bikes from Docking Station to Garage' do
+    it 'removes broken bikes from DockingStation' do
+      station = DockingStation.new(0)
+      broken_bike = double('broken_bike', :working => false)
+      working_bike = double('working_bike', :working => true)
+      3.times { station.dock(working_bike) }
+      2.times { station.dock(broken_bike) }
+      station.ready_for_repair
+      garage = []
+      expect { subject.transport_broken(station, garage) }.to change { station.broken_bikes.length }.by(-2)
+    end
+
+    it 'places broken bikes in Garage' do
       station = DockingStation.new(0)
       broken_bike = double('broken_bike', :working => false)
       working_bike = double('working_bike', :working => true)
